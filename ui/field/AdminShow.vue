b24b6989c57feb51fef1b227a8bcd2e19570516e
Charlie Page <charliepage88@gmail.com>
2020-07-03 10:06:54 -0400

    target blank tweak

--- ui/field/AdminShow.vue
+++ ui/field/AdminShow.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <template v-if="!isMultiple">
-      <a :href="value" target="_blank">
+      <a :href="value" target="_blank" rel="noopener">
         <img :src="value" class="rounded h-16 w-16" />
       </a>
     </template>
@@ -9,7 +9,7 @@
     <template v-if="isMultiple">
       <div class="flex flex-wrap -mx-2">
         <div v-for="(image, index) in images" :key="`image-${index}`" class="w-auto px-2 mb-2">
-          <a :href="image" target="_blank">
+          <a :href="image" target="_blank" rel="noopener">
             <img :src="image" class="rounded h-16 w-16" />
           </a>
         </div>
@@ -20,7 +20,6 @@
 
 <script>
 import get from 'lodash.get'
-import isEmpty from 'lodash.isempty'
 
 export default {
   props: [
